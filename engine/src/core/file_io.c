d42f8a6627d0d0eecd2f76a84abafbbb31660068
cainmartin <cain.martin@gmail.com>
2025-08-19 23:57:40 +0900

    Added file handling

--- engine/src/core/file_io.c
+++ engine/src/core/file_io.c
@@ -2,4 +2,33 @@
 // Created by Cain Martin on 2025/08/07.
 //
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "file_io.h"
+
+char* read_file(const char* path, size_t* size)
+{
+    FILE* file = fopen(path, "r");
+    if (!file) return NULL;
+
+    fseek(file, 0, SEEK_END);
+    const size_t file_size = ftell(file);
+    rewind(file);
+
+    char* buffer = malloc(file_size + 1);
+    if (!buffer)
+    {
+        fclose(file);
+        fprintf(stderr, "Unable to open file %s\n", path);
+        return NULL;
+    }
+
+    fread(buffer, 1, file_size, file);
+
+    buffer[file_size] = '\0';
+    *size = file_size;
+
+    fclose(file);
+
+    return buffer;
+}
--- engine/src/core/file_io.h
+++ engine/src/core/file_io.h
@@ -5,4 +5,8 @@
 #ifndef FILE_IO_H_H
 #define FILE_IO_H_H
 
+#include <stdio.h>
+
+char* read_file(const char* path, size_t* size);
+
 #endif //FILE_IO_H_H
